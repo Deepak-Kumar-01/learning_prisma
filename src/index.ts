f989d58fb6d32e8f1129d63b634ac205adb60544
Deepak Kumar <deepakkumar224336@gmail.com>
2025-05-03 23:30:44 +0530

    relation added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
+import express from 'express';
 import { PrismaClient } from "@prisma/client";
 
+const app = express();
 const client= new PrismaClient();
 
-async function createUser() {
-    await client.user.create({
-        data:{
-            username:"Deepak",
-            password:"123",
-            age:25,
-        }
+app.get("/users",async (req,res)=>{
+    const users=await client.user.findMany();
+    res.send({
+        users
     })
-}
-createUser();
+})
+
+app.get("/todos/:id",async (req,res)=>{
+    const id= req.params.id;
+    const users= await client.user.findFirst({
+        where:{
+            id:parseInt(id)
+        },
+        select:{
+            todos:true
+        }
+    });
+    res.send({
+        users
+    })
+})
+
+app.listen(3000);
+
+// commented out because it will create try to re-insert record
+
+// async function createUser() {
+//     await client.user.create({
+//         data:{
+//             username:"Deepak",
+//             password:"123",
+//             age:25,
+//         }
+//     })
+// }
+// createUser();
